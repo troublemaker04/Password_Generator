6d5c408f9d060f7e3b02d427b088d8711341b7eb
troublemaker04 <124023992+troublemaker04@users.noreply.github.com>
2023-08-29 18:17:21 -0700

    Update pwdgen.py

--- pwdgen.py
+++ pwdgen.py
@@ -20,6 +20,14 @@ password_generator = PasswordGenerator()
 
 password_length = int(input("Enter the desired password length: "))
 password = password_generator.generate_password(password_length)
-print("Generated password:", password)
-print('you have 7 seconds to copy it, COPY IT!')
-time.sleep(7)
+
+if len(password) > 50:
+    with open('generatedpwd.txt','w')as file:
+        file.write(password)
+        print('since ur password was more than 50 charecters long, it was probably to big to fit on one screen, so we just saved it to a file! The file should be called, generatedpwd.')
+        print('exiting in 8 seconds.')
+        time.sleep(8)
+        
+else:
+    print("Generated password:", password)
+    print('you have 7 seconds to copy it, COPY IT!')
