79f4629b7c35359026e93f77dbcb763a3c9cb02d
troublemaker04 <124023992+troublemaker04@users.noreply.github.com>
2023-08-29 18:04:52 -0700

    Add files via upload

---
+++ pwdgen.py
@@ -0,0 +1,23 @@
+import random
+
+class PasswordGenerator:
+    def __init__(self):
+        self.uppercase_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        self.lowercase_letters = 'abcdefghijklmnopqrstuvwxyz'
+        self.numbers = '0123456789'
+        
+    def generate_password(self, length):
+        password = ''
+        
+        for _ in range(length):
+            choice_list = random.choice([self.uppercase_letters, self.lowercase_letters, self.numbers])
+            password += random.choice(choice_list)
+        
+        return password
+
+# Create an instance of PasswordGenerator
+password_generator = PasswordGenerator()
+
+password_length = int(input("Enter the desired password length: "))
+password = password_generator.generate_password(password_length)
+print("Generated password:", password)
